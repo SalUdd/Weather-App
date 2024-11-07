8b20eac83baec3bce328d2612e2f750e4956cdb9
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-11-07 21:12:00 +1100

    inital files - first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+async function getWeather() {
+
+    const weatherData = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=9CDMG2ABREC2PRNZZEKTWAJPJ')
+
+
+
+}
